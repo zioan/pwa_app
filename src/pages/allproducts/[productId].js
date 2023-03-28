8fcb1dbcb4cf0fa46c2418770d57c9bb4900e339
Ioan Zaharia <ioan.zaharia@twt.de>
2023-03-28 15:47:38 +0200

    all products and detail product pages

---
+++ src/pages/allproducts/[productId].js
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import ProductsContext from "../../../context/ProductsContext";
+import { AiFillHeart } from "react-icons/ai";
+
+export default function Product() {
+  const router = useRouter();
+  const { productId } = router.query;
+  const { products } = useContext(ProductsContext);
+
+  const product = products.find(
+    (product) => product.id === parseInt(productId)
+  );
+  console.log(product);
+
+  if (!product) {
+    return <div>No product found</div>;
+  }
+
+  function addProductToWishlist() {
+    localStorage.setItem("wishlist", JSON.stringify(product));
+  }
+
+  return (
+    <div>
+      <div className="d-flex ">
+        <Image
+          src={product.images[0]}
+          className=""
+          alt={product.title}
+          width={350}
+          height={350}
+        />
+        <div>
+          <h1>{product.title}</h1>
+          <h2>$ {product.price}</h2>
+          <button className="btn btn-primary">In der Warenkorb</button>
+          <AiFillHeart onClick={addProductToWishlist} size={40} className="" />
+        </div>
+      </div>
+      <div>
+        <p>{product.description}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/allproducts/index.js
@@ -0,0 +1,7 @@
+import ProductsList from "../../../components/sharedComponents/ProductsList";
+
+function allproducts() {
+  return <ProductsList limit="-1" componentTitle={"Alle Produkte"} />;
+}
+
+export default allproducts;
