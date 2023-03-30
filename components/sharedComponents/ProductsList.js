e07beba7f7b0d09e712305595b0b237650c2eb9c
Ioan Zaharia <ioan.zaharia@twt.de>
2023-03-30 13:26:20 +0200

    filter products by price

--- components/sharedComponents/ProductsList.js
+++ components/sharedComponents/ProductsList.js
@@ -1,36 +1,70 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function ProductsList({ componentTitle, limit, products }) {
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
+
+  function handleSortChange(event) {
+    const selectedValue = event.target.value;
+    if (selectedValue === "ascending") {
+      setFilteredProducts(
+        [...filteredProducts].sort((a, b) => a.price - b.price)
+      );
+    } else if (selectedValue === "descending") {
+      setFilteredProducts(
+        [...filteredProducts].sort((a, b) => b.price - a.price)
+      );
+    }
+  }
+
   return (
     <div className="container mb-5">
-      {componentTitle && <h2>{componentTitle}</h2>}
+      <div className="d-flex justify-content-between align-items-center mb-4">
+        {componentTitle && <h2>{componentTitle}</h2>}
+        {!limit && (
+          <select
+            className="form-select"
+            aria-label="Sort"
+            onChange={handleSortChange}
+          >
+            <option value="">Sortieren Nach</option>
+            <option value="ascending">Preis aufsteigend</option>
+            <option value="descending">Preis absteigend</option>
+          </select>
+        )}
+      </div>
       <div className="featuredContainer">
-        {products &&
-          products.slice(0, limit || products.length).map((product) => {
-            return (
-              <Link href={`/products/${product.id}`} key={product.id}>
-                <div className="card">
-                  {product.images[0] && (
-                    <Image
-                      src={product.thumbnail}
-                      className="card-img-top"
-                      alt={product.title}
-                      width={50}
-                      height={50}
-                    />
-                  )}
-                  <div className="card-body">
-                    <h5 className="card-title">{product.title}</h5>
-                    <p className="card-text">
-                      {product.description.substring(0, 20)}...
-                    </p>
+        {filteredProducts &&
+          filteredProducts
+            .slice(0, limit || filteredProducts.length)
+            .map((product) => {
+              return (
+                <Link href={`/products/${product.id}`} key={product.id}>
+                  <div className="card">
+                    {product.images[0] && (
+                      <Image
+                        src={product.thumbnail}
+                        className="card-img-top"
+                        alt={product.title}
+                        width={50}
+                        height={50}
+                      />
+                    )}
+                    <div className="card-body">
+                      <h5 className="card-title">{product.title}</h5>
+                      <p className="card-text">
+                        {product.description.substring(0, 20)}...
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            );
-          })}
+                </Link>
+              );
+            })}
       </div>
     </div>
   );
